37fec4ced0c075e26d17b52f9ddfa871c18228d4
MalteNord <malte.nordstrand@gmail.com>
2024-11-27 14:03:52 +0100

    Updated CORS Position

--- cms24_delta_umbraco/Program.cs
+++ cms24_delta_umbraco/Program.cs
@@ -16,7 +16,7 @@ builder.Services.AddCors(options =>
             "https://quizify-543tuvkpd-maltes-projects-d53b188c.vercel.app",
             "https://quizify-kappa.vercel.app"
         )
-        .AllowAnyMethod() 
+        .AllowAnyMethod()
         .AllowAnyHeader()
         .AllowCredentials();
     });
@@ -66,7 +66,11 @@ WebApplication app = builder.Build();
 await app.BootUmbracoAsync();
 
 app.UseSession();
-app.UseCors("AllowOrigins");
+
+app.UseRouting(); // UseRouting should come before UseCors
+
+app.UseCors("AllowOrigins"); // Correct position for UseCors
+
 
 app.UseUmbraco()
     .WithMiddleware(u =>
@@ -80,8 +84,11 @@ app.UseUmbraco()
         u.UseWebsiteEndpoints();
     });
 
+// SignalR hubs
 app.MapHub<LobbyHub>("/lobbyHub").RequireCors("AllowOrigins");
 app.MapHub<GameHub>("/gameHub").RequireCors("AllowOrigins");
+
+// Other endpoints
 app.MapControllers();
 
 await app.RunAsync();
