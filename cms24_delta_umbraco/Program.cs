efc44a4b13de7b1fc72b3c4aa3481da4e0de35d6
MalteNord <malte.nordstrand@gmail.com>
2024-11-27 11:00:01 +0100

    changed CORS

--- cms24_delta_umbraco/Program.cs
+++ cms24_delta_umbraco/Program.cs
@@ -9,13 +9,17 @@ WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddCors(options =>
 {
-	options.AddPolicy("AllowLocalhost3000", policy =>
-	{
-		policy.WithOrigins("http://localhost:3000")
-			  .AllowAnyMethod()
-			  .AllowAnyHeader()
-			  .AllowCredentials();
-	});
+    options.AddPolicy("AllowOrigins", policy =>
+    {
+        policy.WithOrigins(
+            "http://localhost:3000",
+            "https://quizify-543tuvkpd-maltes-projects-d53b188c.vercel.app"
+            "https://quizify-kappa.vercel.app"
+        )
+        .AllowAnyMethod()
+        .AllowAnyHeader()
+        .AllowCredentials();
+    });
 });
 
 builder.Services.Configure<SpotifySettings>(builder.Configuration.GetSection("Spotify"));
@@ -23,35 +27,34 @@ builder.Services.AddScoped<ISpotifyService, SpotifyService>();
 builder.Services.AddHttpContextAccessor();
 builder.Services.AddSession(options =>
 {
-	options.IdleTimeout = TimeSpan.FromMinutes(15);
-	options.Cookie.HttpOnly = true;
-	options.Cookie.IsEssential = true;
+    options.IdleTimeout = TimeSpan.FromMinutes(15);
+    options.Cookie.HttpOnly = true;
+    options.Cookie.IsEssential = true;
 });
-
 
 builder.Services.Configure<CookiePolicyOptions>(options =>
 {
-	options.MinimumSameSitePolicy = SameSiteMode.Strict;
-	options.Secure = CookieSecurePolicy.None;
+    options.MinimumSameSitePolicy = SameSiteMode.None;
+    options.Secure = CookieSecurePolicy.Always;
 });
 
 builder.Services.AddScoped<IGameService, GameService>();
 builder.Services.AddHostedService<RoomCleanupService>();
 builder.Services.AddSignalR(options =>
 {
-	options.KeepAliveInterval = TimeSpan.FromSeconds(10);
-	options.ClientTimeoutInterval = TimeSpan.FromMinutes(2);
+    options.KeepAliveInterval = TimeSpan.FromMinutes(1);
+    options.ClientTimeoutInterval = TimeSpan.FromMinutes(5);
 });
 
 builder.Services.AddDbContext<AppDbContext>(options =>
-	options.UseSqlServer(builder.Configuration.GetConnectionString("umbracoDbDSN")));
+    options.UseSqlServer(builder.Configuration.GetConnectionString("umbracoDbDSN")));
 
 builder.CreateUmbracoBuilder()
-	.AddBackOffice()
-	.AddWebsite()
-	.AddDeliveryApi()
-	.AddComposers()
-	.Build();
+    .AddBackOffice()
+    .AddWebsite()
+    .AddDeliveryApi()
+    .AddComposers()
+    .Build();
 
 builder.Logging.ClearProviders();
 builder.Logging.AddConsole();
@@ -62,25 +65,22 @@ WebApplication app = builder.Build();
 await app.BootUmbracoAsync();
 
 app.UseSession();
-
-app.UseCors("AllowLocalhost3000");
-
-
+app.UseCors("AllowOrigins");
 
 app.UseUmbraco()
-	.WithMiddleware(u =>
-	{
-		u.UseBackOffice();
-		u.UseWebsite();
-	})
-	.WithEndpoints(u =>
-	{
-		u.UseBackOfficeEndpoints();
-		u.UseWebsiteEndpoints();
-	});
+    .WithMiddleware(u =>
+    {
+        u.UseBackOffice();
+        u.UseWebsite();
+    })
+    .WithEndpoints(u =>
+    {
+        u.UseBackOfficeEndpoints();
+        u.UseWebsiteEndpoints();
+    });
 
-app.MapHub<LobbyHub>("/lobbyHub");
-app.MapHub<GameHub>("/gameHub");
+app.MapHub<LobbyHub>("/lobbyHub").RequireCors("AllowOrigins");
+app.MapHub<GameHub>("/gameHub").RequireCors("AllowOrigins");
 app.MapControllers();
 
 await app.RunAsync();
